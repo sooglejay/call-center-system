cf4f42d481574bb0d9591ff56e039ba51173f10b
sooglejay <sooglejy@gmail.com>
2026-03-14 15:38:00 +0800

    bugfix: 解决前端项目port被覆盖的问题

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:5004',
+        target: 'http://localhost:3001',  // 后端服务地址
         changeOrigin: true,
         secure: false
       }
